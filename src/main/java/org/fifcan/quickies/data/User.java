e46bf4309647c84f8fc892958717325c9aeb2182
romain <rom1.petit@gmail.com>
2014-12-16 00:13:52 +0100

    add data model

--- src/main/java/org/fifcan/quickies/data/User.java
+++ src/main/java/org/fifcan/quickies/data/User.java
@@ -15,6 +15,11 @@ public class User {
         this.email = email;
     }
 
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
