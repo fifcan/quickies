ebaadb107c97497d0a1e3ce2660402cbe930154c
fifcan <foot0404>
2015-01-18 00:48:20 +0100

    ...

--- src/main/java/org/fifcan/quickies/controller/rest/UserGroupSessionRestController.java
+++ src/main/java/org/fifcan/quickies/controller/rest/UserGroupSessionRestController.java
@@ -1,8 +1,5 @@
 package org.fifcan.quickies.controller.rest;
 
-import com.mongodb.WriteResult;
-import org.fifcan.quickies.data.User;
-import org.fifcan.quickies.data.UserGroup;
 import org.fifcan.quickies.data.UserGroupSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -10,7 +7,6 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,5 +38,34 @@ public class UserGroupSessionRestController {
         mongoTemplate.save(userGroupSession);
         return new ResponseEntity<UserGroupSession>(userGroupSession, HttpStatus.OK);
     }
+
+//    @Autowired
+//    private UserGroupSessionDAO userGroupSessionDAO;
+//
+//    @Autowired
+//    private VoteDAO voteDAO;
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/api/userGroupSession/{id}")
+//    public ResponseEntity<UserGroupSession> getUserGroupSession(@PathVariable(value="id") String id) {
+//        UserGroupSession userGroupSession = userGroupSessionDAO.findById(id);
+//        return new ResponseEntity<UserGroupSession>(userGroupSession, HttpStatus.OK);
+//    }
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/api/userGroupSession")
+//    public ResponseEntity<List<UserGroupSession>> getUserGroupSessions() {
+//        return new ResponseEntity<List<UserGroupSession>>(userGroupSessionDAO.findAll(), HttpStatus.OK);
+//    }
+//
+//    @RequestMapping(method = RequestMethod.POST, value = "/api/userGroupSession")
+//    public ResponseEntity<UserGroupSession> saveUserGroupSession(@RequestBody UserGroupSession userGroupSession) {
+//        userGroupSessionDAO.create(userGroupSession);
+//        return new ResponseEntity<UserGroupSession>(userGroupSession, HttpStatus.OK);
+//    }
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/api/userGroupSession/{id}/vote")
+//    public void voteUserGroupSession(@PathVariable(value="id") String userGroupSessionId) {
+//        Vote vote = new Vote(userGroupSessionId);
+//        voteDAO.create(vote);
+//    }
 
 }
