f13efc0def001f0cad27228d8408de9c43adeebf
romain <rom1.petit@gmail.com>
2015-01-20 22:40:43 +0100

    fix DAO top sessions

--- src/main/java/org/fifcan/quickies/mongo/VoteDao.java
+++ src/main/java/org/fifcan/quickies/mongo/VoteDao.java
@@ -1,22 +1,22 @@
 package org.fifcan.quickies.mongo;
 
-import com.mongodb.*;
+import com.mongodb.AggregationOutput;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import org.apache.log4j.Logger;
-import org.fifcan.quickies.data.UserGroup;
 import org.fifcan.quickies.data.UserGroupSession;
 import org.fifcan.quickies.data.Vote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 /**
@@ -25,8 +25,6 @@ import java.util.stream.StreamSupport;
 public class VoteDao {
 
     private static final Logger LOGGER = Logger.getLogger(VoteDao.class);
-
-    private static final Class<Vote> CLASS = Vote.class;
 
     @Autowired
     protected MongoTemplate mongoTemplate;
@@ -40,7 +38,28 @@ public class VoteDao {
 
         DBCollection collection = mongoTemplate.getCollection("votes");
 
-       // BasicDBObject dateQuery = new BasicDBObject ("$match", new BasicDBObject("eventDate", new BasicDBObject("$gt", todayDate)));
+        DBObject groupFields = new BasicDBObject( "_id", "$userGroupSession");
+        groupFields.put("count", new BasicDBObject( "$sum", 1));
+        DBObject group = new BasicDBObject("$group", groupFields );
+
+        DBObject sortFields = new BasicDBObject("count", -1);
+        DBObject sort = new BasicDBObject("$sort", sortFields );
+
+        AggregationOutput output = collection.aggregate(Arrays.asList(group, sort));
+
+        Iterable<DBObject> results = output.results();
+
+        return getUserGroupSessionsAfter(todayDate, limit, results);
+    }
+
+    public List<UserGroupSession> getTopPastSessions(int limit) {
+
+        LocalDate todayLocalDate = LocalDate.now();
+
+        Instant instant = todayLocalDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+        Date todayDate = Date.from(instant);
+
+        DBCollection collection = mongoTemplate.getCollection("votes");
 
         DBObject groupFields = new BasicDBObject( "_id", "$userGroupSession");
         groupFields.put("count", new BasicDBObject( "$sum", 1));
@@ -53,37 +72,21 @@ public class VoteDao {
 
         Iterable<DBObject> results = output.results();
 
-        return getUserGroupSessions(limit, results);
+        return getUserGroupSessionsBefore(todayDate, limit, results);
     }
 
-    public List<UserGroupSession> getTopPastSessions(int limit) {
-
-        LocalDate todayLocalDate = LocalDate.now();
-
-        Instant instant = todayLocalDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
-        Date todayDate = Date.from(instant);
-
-        DBCollection collection = mongoTemplate.getCollection("votes");
-
-        BasicDBObject dateQuery = new BasicDBObject ("$match", new BasicDBObject("eventDate", new BasicDBObject("$lt", todayDate)));
-
-        DBObject groupFields = new BasicDBObject( "_id", "$userGroupSession");
-        groupFields.put("count", new BasicDBObject( "$sum", 1));
-        DBObject group = new BasicDBObject("$group", groupFields );
-
-        DBObject sortFields = new BasicDBObject("count", -1);
-        DBObject sort = new BasicDBObject("$sort", sortFields );
-
-        AggregationOutput output = collection.aggregate(Arrays.asList(dateQuery, group, sort));
-
-        Iterable<DBObject> results = output.results();
-
-        return getUserGroupSessions(limit, results);
-    }
-
-    private List<UserGroupSession> getUserGroupSessions(int limit, Iterable<DBObject> results) {
+    private List<UserGroupSession> getUserGroupSessionsAfter(Date date, int limit, Iterable<DBObject> results) {
         return StreamSupport.stream(results.spliterator(), false)
                 .map(result -> mongoTemplate.findById(result.get("_id"), UserGroupSession.class))
+                .filter(session -> session.getEventDate().after(date))
+                .limit(limit)
+                .collect(Collectors.toList());
+    }
+
+    private List<UserGroupSession> getUserGroupSessionsBefore(Date date, int limit, Iterable<DBObject> results) {
+        return StreamSupport.stream(results.spliterator(), false)
+                .map(result -> mongoTemplate.findById(result.get("_id"), UserGroupSession.class))
+                .filter(session -> session.getEventDate().before(date))
                 .limit(limit)
                 .collect(Collectors.toList());
     }
