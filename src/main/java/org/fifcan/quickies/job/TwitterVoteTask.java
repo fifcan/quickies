19af94df3030bf89b28d732f074872ac538bb598
fifcan <foot0404>
2015-01-26 20:45:05 +0100

    Blindage de la tache planifiée pour qu'elle n'explose pas la JVM de l'application !

--- src/main/java/org/fifcan/quickies/job/TwitterVoteTask.java
+++ src/main/java/org/fifcan/quickies/job/TwitterVoteTask.java
@@ -1,5 +1,6 @@
 package org.fifcan.quickies.job;
 
+import org.apache.log4j.Logger;
 import org.fifcan.quickies.twitter.TwitterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,11 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class TwitterVoteTask {
 
+    private static final Logger LOGGER = Logger.getLogger(TwitterVoteTask.class);
+
     @Autowired
     TwitterService twitterService;
 
     @Scheduled(fixedRate = 30000L)
     public void loadTwitterVote() {
-        twitterService.processNextTwitterVotes();
+        try {
+            twitterService.processNextTwitterVotes();
+        } catch (Throwable t){
+            LOGGER.warn(t.getMessage());
+        }
     }
 }
